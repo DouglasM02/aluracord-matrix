b768680b6812e6dc567e6b138764c26d68a5d13b
DouglasM02 <55521413+DouglasM02@users.noreply.github.com>
2022-01-29 19:13:17 -0300

    adicionando página de erro

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+
+export default function Custom404() {
+    return (
+        <Box>
+            <Text variant='body1'>
+                Ops, não foi possivel encontrar essa praia!
+            </Text>
+        </Box>
+    )
+}
