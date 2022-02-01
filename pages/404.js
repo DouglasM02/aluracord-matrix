1fa2323e0660c02543f44ef0283ca6995553549d
DouglasM02 <55521413+DouglasM02@users.noreply.github.com>
2022-02-01 13:10:59 -0300

    pagina 404

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,54 @@
 import {
     Box, Button, Text, TextField, Image,
 } from '@skynexui/components';
+import { motion } from 'framer-motion';
+import Lottie from 'react-lottie';
+import appConfig from '../config.json';
+import travellerWalking from '../src/lottie/traveller-walking.json';
 
 export default function Custom404() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: travellerWalking,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+        },
+    };
+
     return (
-        <Box>
-            <Text variant="body1">
-                Ops, não foi possivel encontrar essa praia!
+        <Box
+            styleSheet={{
+                height: '100vh',
+                width: '100vw',
+                display: 'flex',
+                flexDirection: 'column',
+                padding: '30px',
+                backgroundColor: appConfig.theme.colors.primary['700'],
+            }}
+        >
+            <Text
+                styleSheet={{
+                    marginTop: '30px',
+                    marginBottom: '30px',
+                    marginHorizontal: 'auto',
+                    color: appConfig.theme.colors.tertiary['700'],
+                }}
+                variant="body1"
+            >
+                <motion.div
+                    animate={{ rotate: 360 }}
+                    transition={{ duration: 2 }}
+                >
+                    <strong>Ops, não foi possivel encontrar esta praia!</strong>
+                </motion.div>
             </Text>
+
+            <Lottie
+                options={defaultOptions}
+                height={400}
+                width={400}
+            />
         </Box>
     );
 }
